b88e6d10ab53743ad67b3d627e897c84138ff5e3
machajdaa <honzikmachace@seznam.cz>
2026-05-12 20:34:35 +0200

    feat: add app layout and routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import { NAV_ITEMS } from './core/constants/nav-items';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('finance-tracker');
+export class AppComponent {
+  private breakPointObserver = inject(BreakpointObserver);
+  readonly navItems = NAV_ITEMS;
+
+  isMobile = toSignal(
+    this.breakPointObserver.observe(Breakpoints.Handset).pipe(
+      map(result => result.matches)
+    )
+  )
 }
---
+++ src/app/core/constants/nav-items.ts
@@ -0,0 +1,12 @@
+export interface navItem {
+  path: string;
+  icon: string;
+  label: string;
+  isVisibile: boolean;
+}
+
+export const NAV_ITEMS = [
+  { path: "/dashboard", icon: "dashboard", label: "Dashboard", isVisibile: true },
+  { path: "/transactions", icon: "receipt_long", label: "Transakce", isVisibile: true },
+  { path: "/statistics", icon: "bar_chart", label: "Statistiky", isVisibile: true },
+]
